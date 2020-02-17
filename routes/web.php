20ccf09ba04b0c69e60b17bdcc223a545c8896b6
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-02-17 09:20:11 -0300

    :construction: Criando e removendo associações

--- routes/web.php
+++ routes/web.php
@@ -73,3 +73,51 @@ Route::get('/categoriasprodutos/json', function() {
     $cats = Categoria::with('produtos')->get();
     return $cats->toJson();
 });
+
+Route::get('/adicionarproduto', function() {
+
+    $cat = Categoria::find(1);
+
+    $p = new Produto();
+    $p->nome = 'Meu novo Produto';
+    $p->estoque = 10;
+    $p->preco = 100;
+    // O associate() Cria associação através do relacionamento,
+    // Nesse caso a categoria pega o id que está instanciado na linha 79, id 1 no caso.
+    $p->categoria()->associate($cat);
+    $p->save();
+
+    return $p->toJson();
+
+});
+
+Route::get('/adicionarproduto', function() {
+
+    $cat = Categoria::find(1);
+
+    $p = new Produto();
+    $p->nome = 'Meu novo Produto';
+    $p->estoque = 10;
+    $p->preco = 100;
+    // O associate() Cria associação através do relacionamento,
+    // Nesse caso a categoria pega o id que está instanciado na linha 79, id 1 no caso.
+    $p->categoria()->associate($cat);
+    $p->save();
+
+    return $p->toJson();
+
+});
+
+
+Route::get('/removerprodutocategoria', function() {
+
+    $p = Produto::find(12);
+    if(isset($p)) {
+        $p->categoria()->dissociate();
+        $p->save();
+        return $p->toJson();
+    }
+    
+    return '';
+
+});
